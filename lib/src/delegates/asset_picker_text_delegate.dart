7adbc96a7696c9b6a4b3379204232a07b13c1dba
orange-amuz <orange@amuz.co.kr>
2023-06-01 17:17:58 +0900

    한국어 번역 일부 추가

--- lib/src/delegates/asset_picker_text_delegate.dart
+++ lib/src/delegates/asset_picker_text_delegate.dart
@@ -106,10 +106,10 @@ class AssetPickerTextDelegate {
   String durationIndicatorBuilder(Duration duration) {
     const String separator = ':';
     final String minute = duration.inMinutes.toString().padLeft(2, '0');
-    final String second =
-        (duration - Duration(minutes: duration.inMinutes)).inSeconds
-            .toString()
-            .padLeft(2, '0');
+    final String second = (duration - Duration(minutes: duration.inMinutes))
+        .inSeconds
+        .toString()
+        .padLeft(2, '0');
     return '$minute$separator$second';
   }
 
@@ -940,4 +940,98 @@ class VietnameseAssetPickerTextDelegate extends AssetPickerTextDelegate {
 
   @override
   String get sUnitAssetCountLabel => 'số lượng';
+}
+
+class KoreanAssetPickerTextDelegate extends AssetPickerTextDelegate {
+  const KoreanAssetPickerTextDelegate();
+
+  @override
+  String get languageCode => 'kr';
+
+  @override
+  String get confirm => '확인';
+
+  @override
+  String get cancel => '취소';
+
+  @override
+  String get edit => '수정';
+
+  @override
+  String get gifIndicator => 'GIF';
+
+  @override
+  String get loadFailed => '로드 실패';
+
+  @override
+  String get original => '원본';
+
+  @override
+  String get preview => '미리보기';
+
+  @override
+  String get select => '선택';
+
+  @override
+  String get emptyList => '빈 리스트';
+
+  @override
+  String get unSupportedAssetType => 'Unsupported HEIC asset type.';
+
+  @override
+  String get unableToAccessAll => 'Unable to access all assets on the device';
+
+  @override
+  String get viewingLimitedAssetsTip =>
+      'Only view assets and albums accessible to app.';
+
+  @override
+  String get changeAccessibleLimitedAssets =>
+      'Click to update accessible assets';
+
+  @override
+  String get accessAllTip => 'App can only access some assets on the device. '
+      'Go to system settings and allow app to access all assets on the device.';
+
+  @override
+  String get goToSystemSettings => 'Go to system settings';
+
+  @override
+  String get accessLimitedAssets => 'Continue with limited access';
+
+  @override
+  String get accessiblePathName => 'Accessible assets';
+
+  @override
+  String get sTypeAudioLabel => '오디오';
+
+  @override
+  String get sTypeImageLabel => '이미지';
+
+  @override
+  String get sTypeVideoLabel => '비디오';
+
+  @override
+  String get sTypeOtherLabel => '그 외';
+
+  @override
+  String get sActionPlayHint => '재생';
+
+  @override
+  String get sActionPreviewHint => '미리보기';
+
+  @override
+  String get sActionSelectHint => '선택';
+
+  @override
+  String get sActionSwitchPathLabel => 'switch path';
+
+  @override
+  String get sActionUseCameraHint => '카메라 사용';
+
+  @override
+  String get sNameDurationLabel => '기간';
+
+  @override
+  String get sUnitAssetCountLabel => 'count';
 }
